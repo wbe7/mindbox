17a5a599850de808464490dbb16d96f2db8cf28c
Mikhail Tikhonov <wbe7@ya.ru>
2020-02-16 23:22:12 +0300

    Small demo

---
+++ cmd/recive/recive.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	login := os.Getenv("LOGIN")
+	password := os.Getenv("PASSWORD")
+	broker := os.Getenv("BROKER")
+	url := "amqp://" + login + ":" + password + "@" + broker
+	conn, err := amqp.Dial(url)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	queue := os.Getenv("QUEUE")
+	q, err := ch.QueueDeclare(
+		queue, // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
---
+++ cmd/send/send1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendMessage(ch *amqp.Channel, q amqp.Queue, message string) {
+	err := ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(message),
+		})
+	log.Printf(" [x] Sent %s", message)
+	failOnError(err, "Failed to publish a message")
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://wbe7:mindbox123@34.89.173.76:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello1", // name
+		false,    // durable
+		false,    // delete when unused
+		false,    // exclusive
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	for i := 0; i < 1000000; i++ {
+		body := "Hello World!" + strconv.Itoa(i)
+		sendMessage(ch, q, body)
+		time.Sleep(500 * time.Microsecond)
+	}
+}
